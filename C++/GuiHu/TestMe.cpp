c6fa372fc51431898b9142cb0b3f668ab25b6ebf
Guilherme <enshihhu.20231@poli.ufrj.br>
2026-03-19 14:05:04 -0300

    Implement template stack class 'Pilha' in TestMe.cpp
    Added a template class 'Pilha' for stack implementation with methods to push and pop elements. Demonstrated usage of the stack with different data types in the main function.

--- C++/GuiHu/TestMe.cpp
+++ C++/GuiHu/TestMe.cpp
@@ -68,6 +68,23 @@ template <typename T> // class <=> typename
     }
 
 
+template <typename Tipo>
+class Pilha{
+    public:
+        Pilha(): atual(0) { } // inicialização na lista de inicialização, é mais eficiente do que fazer dentro do corpo do construtor; ou você pode fazer {atual = 0}, mas para classes num geral é pior    
+        void empilha( const Tipo& valor ){ // o parâmetro valor de empilha se adapta ao tipo usado; const ref porque pilha de pilha pode ser grantde
+            tab[atual++] = valor;
+        }
+        Tipo desempilha(){         // o retorno do desempilha tambem
+            return tab[--atual];
+        }
+
+    private:
+        const static int MAX_PILHA = 10; // const static para ser uma constante de classe, ou seja, compartilhada por todas as instâncias da classe
+        Tipo tab[MAX_PILHA]; // o tipo do vetor se adapta ao tipo usado
+        int atual;
+};
+
 int main(){
     PilhaInt q;
     q.empilha(5);
@@ -87,6 +104,16 @@ int main(){
     cout << Max<int>(3.14, 2.71) << endl;  
     cout << Max<double>(3.14, 4) << endl;  
     cout << Max("abc", "xyz") << endl;
+
+
+    Pilha<int> p1;
+    Pilha<double> p2;
+    Pilha< Pilha<int> > p3;
+
+    p1.empilha(8);
+    p2.empilha(11.5);
+    p3.empilha(p1); // não é possível empilhar p2 pois isso é uma pilha de pilhas Instância
+
 
 }
 
